6eb5551db3d893c82921afe1050553c5c83132d0
ZaneyH <90028324+ZaneyH@users.noreply.github.com>
2021-09-10 09:52:51 +0200

    Create checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,74 @@
+function summa () {
+       console.log(5+6)
+}
+
+
+let rahakott = 10
+let tootehind = 5
+ if (rahakott >= tootehind)
+     rahakott = 30
+     rahakott =rahakott - tootehind
+     rahakott += 10
+     
+     
+     let summa = function () {
+           
+           }
+           
+           //primitive types:
+           // number string boolean object function undefined null
+           // user defined types:
+           //Math
+           
+           TYPEOF= nätab mis tüüpi infot on muutuja sees
+           
+           Let töötaja = {nimi: "Henri", palk:10 }
+           let töötaja2= {
+           nimi: "Andrus"
+           palk: 23
+           rahakott: 5
+           maksaPalka: function () {
+            rahakott += palk 
+            }
+           }
+           
+         console.log(töötaja2.nimi)
+         console.log(töötaja2.rahakott)
+         let muuda = "palk"
+         töötaja2.muuda += 5
+         töötaja2["palk"] += 5
+         
+         
+/* 
+katjal on kolm temperatuurivahemikku 
+0-20 on liiga külm     tagastab  -1
+21-40 on paras temperatuur    tagastab 0
+41-60 on liiga kuum   tagastab 1
+*/
+function checkTemp() {
+      let temp = 5;     
+if (temp < 0){
+ console.log("liiga külm")
+  }  else if (temp > 20 && temp <= 40) {
+
+  } else {
+
+  }
+
+
+
+
+}
+function kasKütta(temp) {
+    return temp < 20 
+}
+
+function kasJahutada(temp)  {
+    return temp > 40
+}
+
+let kasJahutada = true;
+if (kasJahutada(44)) {
+
+
+}            
